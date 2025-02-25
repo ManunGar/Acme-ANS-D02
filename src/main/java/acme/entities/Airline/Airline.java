4778aeb6989c7a5316573fa7f235888a760ddf70
kawayypro <juanantoniorrl2004@gmail.com>
2025-02-25 20:25:49 +0100

    Airline Entity

--- src/main/java/acme/entities/Airline/Airline.java
+++ src/main/java/acme/entities/Airline/Airline.java
@@ -1,8 +1,9 @@
 
-package acme.entities.Reviews;
+package acme.entities.Airline;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -12,17 +13,19 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidScore;
 import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Review extends AbstractEntity {
+public class Airline extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
+
+	//Attributes
 
 	@Mandatory
 	@ValidString(max = 50)
@@ -30,27 +33,32 @@ public class Review extends AbstractEntity {
 	private String				name;
 
 	@Mandatory
+	@ValidString(max = 3, pattern = "^[A-Z]+$")
+	@Column(unique = true)
+	private String				IATAcode;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Mandatory
+	@Automapped
+	private Type				type;
+
+	@Mandatory
 	@ValidMoment(past = true)
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date				moment;
-
-	@Mandatory
-	@ValidString(max = 50)
-	@Automapped
-	private String				subject;
-
-	@Mandatory
-	@ValidString(max = 255)
-	@Automapped
-	private String				text;
+	@Temporal(TemporalType.DATE)
+	private Date				foundationMoment;
 
 	@Optional
-	@ValidScore
-	@Automapped
-	private double				score;
+	@ValidString
+	@Column(unique = true)
+	private String				emailAddress;
 
 	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
 	@Automapped
-	private boolean				recommended;
+	private String				phoneNumber;
 
 }

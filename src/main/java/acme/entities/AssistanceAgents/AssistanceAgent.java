f63a2ba2bf29b1cfc794726fc9c3b5245a8f1240
kawayypro <juanantoniorrl2004@gmail.com>
2025-02-27 12:52:39 +0100

    Fix Entity AssistanceAgent

--- src/main/java/acme/entities/AssistanceAgents/AssistanceAgent.java
+++ src/main/java/acme/entities/AssistanceAgents/AssistanceAgent.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
@@ -24,7 +24,7 @@ import lombok.Setter;
 @Entity
 @Setter
 @Getter
-public class AssistanceAgent extends AbstractEntity {
+public class AssistanceAgent extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 

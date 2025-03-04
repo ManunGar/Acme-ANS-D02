9c0f7cfedf29c05a2a03f3c09659c854de41ce3c
kawayypro <juanantoniorrl2004@gmail.com>
2025-03-04 13:44:35 +0100

    Dependencies and quick fixes

--- src/main/java/acme/entities/Claims/Claim.java
+++ src/main/java/acme/entities/Claims/Claim.java
@@ -4,6 +4,7 @@ package acme.entities.Claims;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -12,6 +13,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import acme.entities.AssistanceAgents.AssistanceAgent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,6 +46,13 @@ public class Claim extends AbstractEntity {
 
 	@Mandatory
 	@Automapped
-	private boolean				indicator;
+	private boolean				accepted;	//Indicator
+
+	// Relationships
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private AssistanceAgent		assistanceAgent;
 
 }

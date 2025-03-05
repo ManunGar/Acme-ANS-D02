4d98bc71a1827c7b826aa02593a39e60cec70f42
kawayypro <juanantoniorrl2004@gmail.com>
2025-03-05 11:57:57 +0100

    quick fix AssistanceAgentDashboard

--- src/main/java/acme/forms/AssistanceAgentDashboard.java
+++ src/main/java/acme/forms/AssistanceAgentDashboard.java
@@ -2,7 +2,11 @@
 package acme.forms;
 
 import acme.client.components.basis.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class AssistanceAgentDashboard extends AbstractForm {
 
 	// Serialisation version --------------------------------------------------

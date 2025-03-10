480cd48d04266028715b172b708de4065aef6dd7
juamorrio <jmorenorios5@gmail.com>
2025-03-10 10:14:12 +0100

    Implemented relationships in Review and a Review custom validator

--- src/main/java/acme/entities/Reviews/Review.java
+++ src/main/java/acme/entities/Reviews/Review.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -14,12 +15,17 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidReview;
+import acme.entities.Airlines.Airline;
+import acme.entities.Airports.Airport;
+import acme.entities.Services.Service;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidReview
 public class Review extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
@@ -52,5 +58,20 @@ public class Review extends AbstractEntity {
 	@Optional
 	@Automapped
 	private boolean				recommended;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Airline				airline;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Airport				airport;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Service				service;
 
 }

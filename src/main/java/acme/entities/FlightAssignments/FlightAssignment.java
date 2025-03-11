20880e306ee52ad8abd43f835a7483583da04188
aloporsan <alonsoportillo.sanchez@gmail.com>
2025-03-11 13:23:38 +0100

    D02-I3-4: FA Entity

---
+++ src/main/java/acme/entities/FlightAssignments/FlightAssignment.java
@@ -0,0 +1,60 @@
+
+package acme.entities.FlightAssignments;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.entities.FlightCrewMembers.FlightCrewMember;
+import acme.entities.Legs.Legs;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FlightAssignment extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Duty				duty;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Status				status;
+
+	@Optional
+	@ValidString(min = 0, max = 255)
+	@Automapped
+	private String				remarks;
+
+	@Mandatory
+	@ManyToOne(optional = false)
+	@Valid
+	private FlightCrewMember	flightCrewMember;
+
+	@Mandatory
+	@ManyToOne
+	@Valid
+	private Legs				leg;
+
+}

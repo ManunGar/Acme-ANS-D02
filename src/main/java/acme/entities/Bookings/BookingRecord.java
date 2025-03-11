d657b0bdffae48793edbd63c4a687e08f29687fe
juamorrio <jmorenorios5@gmail.com>
2025-03-11 08:52:41 +0100

    Eliminated custom validator anf innecesary attribute

--- src/main/java/acme/entities/Bookings/BookingRecord.java
+++ src/main/java/acme/entities/Bookings/BookingRecord.java
@@ -1,14 +1,10 @@
 
 package acme.entities.Bookings;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidString;
-import acme.constraints.ValidBookingRecord;
 import acme.entities.Passengers.Passenger;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +12,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@ValidBookingRecord
 public class BookingRecord extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
@@ -26,10 +21,5 @@ public class BookingRecord extends AbstractEntity {
 
 	@ManyToOne(optional = false)
 	private Passenger			passenger;
-
-	@Optional //esto hay que ponerlo?? y va aqui??
-	@ValidString //cuanto??
-	@Column(unique = true)
-	private String				seatNumber;
 
 }

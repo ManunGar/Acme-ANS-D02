6af5e13190c1ae139a4c7598ecfe80df490cde40
ManunGar <nunogarciamanuel@gmail.com>
2025-03-13 11:42:33 +0100

    Customs Validators

--- src/main/java/acme/realms/AirlineManager.java
+++ src/main/java/acme/realms/AirlineManager.java
@@ -16,12 +16,14 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidManager;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidManager
 public class AirlineManager extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
